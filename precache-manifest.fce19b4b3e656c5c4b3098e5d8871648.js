3afeaa83323f6264765517e70cf115eb71a607bd
bauer96 <zocken210@web.de>
2022-05-19 00:10:45 +0200

    Updates

---
+++ precache-manifest.fce19b4b3e656c5c4b3098e5d8871648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "139a017f61c285729888",
+    "url": "/jamming-app/static/css/main.e330898f.chunk.css"
+  },
+  {
+    "revision": "139a017f61c285729888",
+    "url": "/jamming-app/static/js/main.139a017f.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/jamming-app/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "2f1ba0a32e33fdff100b",
+    "url": "/jamming-app/static/js/2.2f1ba0a3.chunk.js"
+  },
+  {
+    "revision": "0223b54ccf35b8b75e8c",
+    "url": "/jamming-app/static/js/runtime~main.0223b54c.js"
+  },
+  {
+    "revision": "e830f4d9d1d5f793f311bc5c143621f5",
+    "url": "/jamming-app/index.html"
+  }
+];
